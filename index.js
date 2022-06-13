bfe7d2135de89b193d82c03edc2956033d3bb020
Manuel <mannisuso@gmail.com>
2022-06-13 12:35:52 -0500

    Finished doing navbar and responsive menu

---
+++ index.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector(".navbar__hamburger");
+const navMenu = document.querySelector(".navbar__list");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".item--link").forEach(n => n.
+addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");   
+}))
