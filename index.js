d9847c005efb6dc27d4d87f7f0f49f51e6329940
Manuel <mannisuso@gmail.com>
2022-06-17 11:57:39 -0500

    When the user opens nav, the content blurs

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const hamburger = document.querySelector(".navbar__hamburger");
 const navMenu = document.querySelector(".navbar__list");
+/* const body = document.querySelector("body"); */
+const main = document.querySelector("main");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
+    /* body.classList.toggle("active"); */
+    main.classList.toggle("active");
 })
 
 document.querySelectorAll(".item--link").forEach(n => n.
 addEventListener("click", () => {
     hamburger.classList.remove("active");
-    navMenu.classList.remove("active");  
+    navMenu.classList.remove("active"); 
+    /* body.classList.remove("active");  */
+    main.classList.remove("active"); 
 }))
