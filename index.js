13f11ca232d0fd0b7d244dee1bd3f8a8e6f8d373
Manuel <mannisuso@gmail.com>
2022-06-17 11:58:48 -0500

    When user opens nav menu, he can't scroll

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const hamburger = document.querySelector(".navbar__hamburger");
 const navMenu = document.querySelector(".navbar__list");
-/* const body = document.querySelector("body"); */
+const body = document.querySelector("body");
 const main = document.querySelector("main");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
-    /* body.classList.toggle("active"); */
+    body.classList.toggle("active");
     main.classList.toggle("active");
 })
 
@@ -14,6 +14,6 @@ document.querySelectorAll(".item--link").forEach(n => n.
 addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active"); 
-    /* body.classList.remove("active");  */
+    body.classList.remove("active"); 
     main.classList.remove("active"); 
 }))
